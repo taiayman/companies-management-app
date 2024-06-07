ff919645b9ff868229a8a21440959b66db8bff5d
taiayman <167556938+taiayman@users.noreply.github.com>
2024-06-07 21:59:06 +0100

    Update theme.dart

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,7 +5,9 @@ ThemeData appTheme(bool isDarkTheme) {
       ? ThemeData.dark().copyWith(
           primaryColor: Colors.black,
           hintColor: Color(0xFFD97757),
-          backgroundColor: Color(0xFF2C2B28),
+          colorScheme: ColorScheme.dark(
+            background: Color(0xFF2C2B28),
+          ),
           bottomNavigationBarTheme: BottomNavigationBarThemeData(
             backgroundColor: Colors.black,
             selectedItemColor: Color(0xFFD97757),
@@ -15,7 +17,9 @@ ThemeData appTheme(bool isDarkTheme) {
       : ThemeData.light().copyWith(
           primaryColor: Colors.white,
           hintColor: Color(0xFFD97757),
-          backgroundColor: Color(0xFFF2F0E8),
+          colorScheme: ColorScheme.light(
+            background: Color(0xFFF2F0E8),
+          ),
           bottomNavigationBarTheme: BottomNavigationBarThemeData(
             backgroundColor: Colors.white,
             selectedItemColor: Color(0xFFD97757),
